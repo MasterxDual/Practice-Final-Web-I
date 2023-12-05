851088a525fbb7704557aa916b4ceb62a1b86e21
Agustin Brambilla <agustinbram@gmail.com>
2023-12-05 14:22:21 -0300

    Minor changes

--- MaquinaAutomatica/CapacidadExcedidaException.java
+++ MaquinaAutomatica/CapacidadExcedidaException.java
@@ -2,8 +2,4 @@
  * Excepcion para falla en capacidad del recipiente
  */
 public class CapacidadExcedidaException extends Exception {
-    //Agregado este metodo por mi
-    public CapacidadExcedidaException(String s) {
-        super(s);
-    }
 }
